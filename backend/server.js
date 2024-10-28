5ecb37be9e5f7aa07e6583373801749790129291
nikhillohra <nikhillohra99@gmail.com>
2024-10-28 16:27:22 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -48,9 +48,9 @@ app.use((err, req, res, next) => {
 
 // Serve frontend in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+  app.use(express.static(path.join(__dirname, "Hackathon", "frontend", "dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "Hackathon", "frontend", "dist", "index.html"));
   });
 }
 
